3fa9b4fd4c7b82fe5e9352c21960439393ffe8a9
Mikita-Kharitonau <khari-vitalij@mail.ru>
2019-10-06 10:49:26 +0300

    SAIO: lab4: removed redundant lines from ResourceAllocationTask

--- saio/lab4/src/main/scala/com/saio/lab4/tasks/ResourceAllocationTask.scala
+++ saio/lab4/src/main/scala/com/saio/lab4/tasks/ResourceAllocationTask.scala
@@ -3,9 +3,7 @@ package com.saio.lab4.tasks
 import breeze.linalg._
 
 class ResourceAllocationTask(F: DenseMatrix[Int]) {
-  val (c, n) = (F.cols, F.rows)
-
-  def solve: (Int, List[Int]) = BellmanFunction(n, c - 1)
+  def solve: (Int, List[Int]) = BellmanFunction(F.rows, F.cols - 1)
 
   def BellmanFunction(i: Int, y: Int): (Int, List[Int]) = {
     if (i == 1) return (F(0, y), List(y))
